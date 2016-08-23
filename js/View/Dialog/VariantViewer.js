6015ce5b1fc996dc55c1cab4824b74d08aff6c81
Colin <colin.diesh@gmail.com>
2016-08-23 14:06:59 -0500

    Add variantviewer

---
+++ js/View/Dialog/VariantViewer.js
@@ -0,0 +1,17 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'dijit/Dialog'
+],
+function(
+    declare,
+    array,
+    Dialog
+) {
+    return declare(Dialog, {
+
+        constructor: function() {
+        }
+
+    });
+})
