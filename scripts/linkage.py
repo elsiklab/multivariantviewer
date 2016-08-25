b572535b63e37b9d799a87400b2c4dc8f55a1951
Colin <colin.diesh@gmail.com>
2016-08-25 05:33:39 -0500

    Add linkage calculations script

---
+++ scripts/linkage.py
@@ -0,0 +1,57 @@
+from toolshed import nopen
+import sys
+import os
+import glob
+import tempfile
+
+def main(args=sys.argv[1:]):
+    region = args[0].replace("chr", "")
+    chrom = "chr%s" % region.split(":")[0]
+
+    ld_snp = args[1] if len(args) > 1 else None
+
+    vcf = pull_vcf(chrom, region)
+    for i, line in enumerate(open(gen_ld(vcf, ld_snp))):
+        if i == 0: line = "#" + line.strip()
+        print "\t".join(line.strip().split())
+    os.unlink(vcf)
+    for pf in glob.glob("%s.%s"):
+        os.unlink(pf)
+
+def gen_ld(vcf, ld_snp):
+    print >>sys.stderr, "calculating LD..."
+    out = tempfile.mktemp(suffix=".ld.txt")
+    rs_list = tempfile.mktemp(suffix=".rs.txt")
+    seen = {}
+    with open(rs_list, "w") as fhrs:
+        j = 0
+        for toks in (line.rstrip().split("\t") for line in open(vcf)):
+            if toks[0][0] == "#": continue
+            if toks[2] in seen: continue
+            seen[toks[2]] = True
+            fhrs.write("%s\n" % toks[2])
+            j += 1
+        print >>sys.stderr, j, "SNPs"
+    cmd = "|plink2 --vcf %s --r2 --ld-window-kb 10000 --ld-window 99999 --ld-window-r2 0"
+    cmd += " --out %s "
+    if ld_snp is None:
+        cmd += " --inter-chr --ld-snp-list %s"
+        cmd %= (vcf, out, rs_list)
+    else:
+        cmd += "--ld-snp %s "
+        cmd %= (vcf, out, ld_snp)
+    list(nopen(cmd))
+    return out + ".ld"
+
+
+def pull_vcf(chrom, region):
+    print >>sys.stderr, "downloading with tabix"
+    tmpvcf = tempfile.mktemp(suffix=".vcf")
+    cmd = "|tabix -p vcf -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20110521/ALL.%s.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz %s > %s" \
+            % (chrom, region, tmpvcf)
+    list(nopen(cmd))
+    return tmpvcf
+
+
+if __name__ == "__main__":
+    main()
